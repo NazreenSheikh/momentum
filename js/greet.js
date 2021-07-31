e590a15a6be90d54b0ba0a73f801333430a28ffa
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-07-31 18:40:44 +0530

    This is a new commit for what I originally planned to be amended

--- js/greet.js
+++ js/greet.js
@@ -1,30 +1,27 @@
-const loginform = document.getElementById("form-input");
-const input = loginform.querySelector(".user-input");
-const greet = document.querySelector(".greet")
+const loginform = document.getElementById('form-input')
+const input = loginform.querySelector('.user-input')
+const greet = document.querySelector('.greet')
 
-
-const HIDDEN_CLASS = "hidden";
-const USERNAME_KEY = "username"
+const HIDDEN_CLASS = 'hidden'
+const USERNAME_KEY = 'username'
 
 const savedUser = localStorage.getItem(USERNAME_KEY)
 if (savedUser === null) {
-    loginform.classList.remove(HIDDEN_CLASS);
-    loginform.addEventListener("submit", subForm);
-
-
+  loginform.classList.remove(HIDDEN_CLASS)
+  loginform.addEventListener('submit', subForm)
 } else {
-    Greetings(savedUser)
+  Greetings(savedUser)
 }
 
 function subForm(e) {
-    e.preventDefault();
-    loginform.classList.add(HIDDEN_CLASS);
-    const username = input.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    Greetings(username)
+  e.preventDefault()
+  loginform.classList.add(HIDDEN_CLASS)
+  const username = input.value
+  localStorage.setItem(USERNAME_KEY, username)
+  Greetings(username)
 }
 
 function Greetings(username) {
-    greet.innerText = `hello ${username}`;
-    greet.classList.remove(HIDDEN_CLASS)
+  greet.innerText = `hello ${username}`
+  greet.classList.remove(HIDDEN_CLASS)
 }
