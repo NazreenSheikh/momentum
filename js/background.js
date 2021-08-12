821839ed7caf4e9f2eecd75bee604a31030eda7d
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-12 21:53:50 +0530

    update this file

--- js/background.js
+++ js/background.js
@@ -1,5 +1,10 @@
+const docBody = document.getElementById('docBody')
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg']
 const chosenImg = images[Math.floor(Math.random() * images.length)]
+if (chosenImg === '02.jpg' || chosenImg === '03.jpg') {
+  docBody.style.color = 'white'
+}
+
 const bgImg = document.createElement('img')
 bgImg.src = `/${chosenImg}`
 document.body.appendChild(bgImg)
--- js/greet.js
+++ js/greet.js
@@ -4,24 +4,29 @@ const greet = document.querySelector('.greet')
 
 const HIDDEN_CLASS = 'hidden'
 const USERNAME_KEY = 'username'
+const FSTYLE_CLASS = 'fstyle'
 
 const savedUser = localStorage.getItem(USERNAME_KEY)
 if (savedUser === null) {
   loginform.classList.remove(HIDDEN_CLASS)
+  loginform.classList.add(FSTYLE_CLASS)
   loginform.addEventListener('submit', subForm)
 } else {
+  loginform.classList.remove(FSTYLE_CLASS)
   Greetings(savedUser)
 }
 
 function subForm(e) {
   e.preventDefault()
   loginform.classList.add(HIDDEN_CLASS)
+  loginform.classList.remove(FSTYLE_CLASS)
+
   const username = input.value
   localStorage.setItem(USERNAME_KEY, username)
   Greetings(username)
 }
 
 function Greetings(username) {
-  greet.innerText = `hello ${username}`
+  greet.innerText = `hello ${username} :)`
   greet.classList.remove(HIDDEN_CLASS)
 }
