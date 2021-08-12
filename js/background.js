21e8b0da8be068b9400a0dcba21e289b71d74b02
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-12 23:20:36 +0530

    bg change

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 const docBody = document.getElementById('docBody')
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg']
 const chosenImg = images[Math.floor(Math.random() * images.length)]
-if (chosenImg === '04.jpg') {
+if (chosenImg === '04.jpg' || chosenImg === '03.jpg') {
   docBody.style.color = 'black'
 }
 
